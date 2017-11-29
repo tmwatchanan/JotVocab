eeb2ae454288a4592900fda538990f66e3676a18
tmwatchanan <tmwatchanan@gmail.com>
2017-11-29 23:27:07 +0700

    Test send GET request to oxford api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,19 +8,23 @@
         <!-- </ul> -->
         <!-- <img :src="user.photoURL" style="height: 120px"> <br>  -->
         <!-- <pre>current user: {{user}}</pre> -->
+        <hr/>
+        <pre>{{ this.entry }}</pre>
     </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import firebase from "firebase";
-import router from '../router';
+import router from "../router";
+import oxfordAPIConfig from "../helpers/apiConfigs";
 
 export default {
   name: "hello",
   data() {
     return {
-      msg: "Welcome to JotVocab App"
+      msg: "Welcome to JotVocab App",
+      entry: ""
     };
   },
   computed: {
@@ -34,14 +38,27 @@ export default {
         .auth()
         .signOut()
         .then(this.onSignOut, this.onError);
-      router.go({ path: '/', force: true })
+      router.go({ path: "/", force: true });
     },
     onSignOut() {
       console.log("onSignOut");
     },
     onError() {
       console.error("onError");
+    },
+    fetchData() {
+      this.$http
+        .get(
+          "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/tommy",
+        )
+        .then(response => {
+          console.log(response.json());
+          return response.json(); // return an a javascript object
+        });
     }
+  },
+  created() {
+    this.fetchData();
   }
 };
 </script>
---
+++ src/helpers/apiConfigs.js
@@ -0,0 +1,4 @@
+export const oxfordAPIConfig = {
+    appId: "1e3b649a",
+    appKey: "f8930cdc2a1bb0f5f577bb0e179c0dde"
+};
