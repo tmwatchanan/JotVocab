b19eeb9b608a3eff52132492e0f9c11edd9ff22e
APICHAT KHADSEETALEE <apichat_k@cmu.ac.th>
2017-12-01 17:07:30 +0700

    Add Word Definition

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,13 @@
         <!-- <pre>current user: {{user}}</pre> -->
         <hr/>
         Search word: <input @keyup.enter="searchWordInDict" v-model.trim="searchWord">
-        <pre>{{ this.entry }}</pre>
+        <div>
+          <h1>{{ this.entry.word }}</h1>
+          <h2>Definition : </h2>
+          <ol id="definition_list">
+          </ol>
+        </div>
+        <!-- <pre>{{ this.entry.results }}</pre> -->
     </div>
 </template>
 
@@ -50,19 +56,26 @@ export default {
     },
     searchWordInDict() {
       this.$http
-        .get(
-          "https://wordsapiv1.p.mashape.com/words/" + this.searchWord + "/definition",
-          {
-            headers: {
-              'X-Mashape-Key': wordAPIConfig.key
-            }
+        .get("https://wordsapiv1.p.mashape.com/words/" + this.searchWord, {
+          headers: {
+            "X-Mashape-Key": wordAPIConfig.key
           }
-        )
+        })
         .then(response => {
           return response.json(); // return an a javascript object
         })
         .then(data => {
           this.entry = data;
+          var ul = document.getElementById("definition_list");
+          ul.innerHTML = "";
+          for (var i = 0; i < this.entry.results.length; i++) {
+            var li = document.createElement("li");
+            var result = this.entry.results[i];
+            // li.appendChild(document.createTextNode("[" + result.partOfSpeech + "] " + result.definition));
+            li.innerHTML = '<em style="color: red">[' + result.partOfSpeech + ']</em> ' + result.definition;
+            li.setAttribute("id", "def" + i); // added line
+            ul.appendChild(li);
+          }
         });
     }
   },
