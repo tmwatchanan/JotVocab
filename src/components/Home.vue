92c224a1774456ddaebfa36352e98cdb0c0dc452
APICHAT KHADSEETALEE <apichat_k@cmu.ac.th>
2017-12-02 19:30:14 +0700

    get data to post

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,14 +9,14 @@
         <!-- <img :src="user.photoURL" style="height: 120px"> <br>  -->
         <!-- <pre>current user: {{user}}</pre> -->
         <hr/>
-        Search word: <input @keyup.enter="searchWordInDict" v-model.trim="searchWord">
-        <div>
-          <h1>{{ this.entry.word }}</h1>
-          <h2>Definition : </h2>
-          <ol id="definition_list">
-          </ol>
+        Search word : <input @keyup.enter="searchWordInDict" v-model.trim="searchWord">
+        <div >
+          <span style="display: inline-flex;"><h1>{{ this.entry.word }}</h1>  <input @click="playsound" type="image" src="http://www.freeiconspng.com/uploads/speaker-icon-27.png" value="Play" style="width: 30px; height: 30px;" /></span>
+          <table id="definition_table">
+          </table>
         </div>
-        <!-- <pre>{{ this.entry.results }}</pre> -->
+        Add some comment : <input type="text" id="comment" name="comment">
+        <button @click="submitdata">Submit</button>
     </div>
 </template>
 
@@ -66,17 +66,86 @@ export default {
         })
         .then(data => {
           this.entry = data;
-          var ul = document.getElementById("definition_list");
-          ul.innerHTML = "";
+          var tb = document.getElementById("definition_table");
+          tb.innerHTML =
+            "<tr><td>No.</td><td>Definition</td><td>Synonym</td><td>Examples</td></tr>";
           for (var i = 0; i < this.entry.results.length; i++) {
-            var li = document.createElement("li");
             var result = this.entry.results[i];
-            // li.appendChild(document.createTextNode("[" + result.partOfSpeech + "] " + result.definition));
-            li.innerHTML = '<em style="color: red">[' + result.partOfSpeech + ']</em> ' + result.definition;
-            li.setAttribute("id", "def" + i); // added line
-            ul.appendChild(li);
+
+            var tr = document.createElement("tr");
+            tr.setAttribute("id", "row" + i);
+            tb.appendChild(tr);
+
+            var n = document.createElement("td");
+            n.innerHTML = i + 1;
+            document.getElementById("row" + i).appendChild(n);
+
+            var def = document.createElement("td");
+            if (result.partOfSpeech != null) {
+              def.innerHTML =
+                '<em style="color: red">[' + result.partOfSpeech + "]</em> ";
+            }
+            def.innerHTML += result.definition;
+            document.getElementById("row" + i).appendChild(def);
+
+            var syn = document.createElement("td");
+            syn.setAttribute("id", "syn" + i);
+            document.getElementById("row" + i).appendChild(syn);
+
+            var syntb = document.createElement("table");
+            syntb.setAttribute("id", "syntb" + i);
+            document.getElementById("syn" + i).appendChild(syntb);
+
+            if (result.synonyms != null) {
+              var synno = 0;
+              if (result.synonyms.length < 3) synno = result.synonyms.length;
+              else synno = 3;
+              for (var j = 0; j < synno; j++) {
+                var syntr = document.createElement("tr");
+                syntr.setAttribute("id", "syntr" + i + j);
+                document.getElementById("syntb" + i).appendChild(syntr);
+
+                var syntd = document.createElement("td");
+                syntd.appendChild(document.createTextNode(result.synonyms[j]));
+                document.getElementById("syntr" + i + j).appendChild(syntd);
+              }
+            }
+
+            var ex = document.createElement("td");
+            ex.setAttribute("id", "ex" + i);
+            document.getElementById("row" + i).appendChild(ex);
+
+            var extb = document.createElement("table");
+            extb.setAttribute("id", "extb" + i);
+            document.getElementById("ex" + i).appendChild(extb);
+
+            if (result.examples != null) {
+              var exno = 0;
+              if (result.examples.length < 3) exno = result.examples.length;
+              else exno = 3;
+              for (var j = 0; j < exno; j++) {
+                var extr = document.createElement("tr");
+                extr.setAttribute("id", "extr" + i + j);
+                document.getElementById("extb" + i).appendChild(extr);
+
+                var extd = document.createElement("td");
+                extd.appendChild(document.createTextNode(result.examples[j]));
+                document.getElementById("extr" + i + j).appendChild(extd);
+              }
+            }
           }
         });
+    },
+    playsound() {
+      responsiveVoice.speak(this.entry.word);
+    },
+    submitdata() {
+      var Data = {
+        uid: this.user.uid,
+        word: this.entry.word,
+        comment: document.getElementById("comment").value
+      };
+      console.log(Data);
     }
   },
   created() {
